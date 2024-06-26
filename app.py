5dfbbae070d2c875888b5e69b0c2eea8ce5109aa
Mncedisi Mncwabe <67195600+MNCEDISIMNCWABE@users.noreply.github.com>
2024-06-26 16:00:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
+import os
+import logging
+from datadog import initialize
 from flask import Flask, jsonify, request
 from recommendation import generate_recommendations, get_last_played_game
 
-app = Flask(__name__)
+# Datadog configuration
+options = {
+    'api_key': '99ff0fb1ea7215302a0338860fa9d373',
+    'app_key': '46a2b52041682068d03a8b49b152083b209141b5'
+}
+
+initialize(**options)
 
 # Configure logging
-import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-app.logger.setLevel(logging.INFO)
+logger = logging.getLogger(__name__)
+
+app = Flask(__name__)
 
 try:
     recommendations_df = generate_recommendations()
 except Exception as e:
-    app.logger.error(f"Error generating recommendations: {e}")
+    logger.error(f"Error generating recommendations: {e}")
     recommendations_df = None
 
 @app.route('/recommend', methods=['POST'])
@@ -34,12 +44,12 @@ def recommend():
 
         last_played_game = get_last_played_game(user_id)
         if not last_played_game:
-            app.logger.error(f"No last played game found for user '{user_id}'")
+            logger.error(f"No last played game found for user '{user_id}'")
             return jsonify({"error": f"No last played game found for user '{user_id}'"}), 404
 
         user_recommendations = recommendations_df[recommendations_df['user_id'] == user_id]
         if user_recommendations.empty:
-            app.logger.error(f"No recommendations found for user '{user_id}'")
+            logger.error(f"No recommendations found for user '{user_id}'")
             return jsonify({"error": f"No recommendations found for user '{user_id}'"}), 404
 
         recommendations = user_recommendations.to_dict(orient='records')
@@ -48,7 +58,7 @@ def recommend():
             "recommendations": recommendations
         })
     except Exception as e:
-        app.logger.error(f"Error in recommendation: {e}")
+        logger.error(f"Error in recommendation: {e}")
         return jsonify({"error": "Internal server error"}), 500
 
 if __name__ == '__main__':
